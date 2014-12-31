080a31aa45f930eaa36a2d83ee3dd1d096a6c2ec
Arun p <arunpadyan@gmail.com>
2014-12-31 17:20:38 +0530

    edited config.java

--- android/src/main/java/com/saarang/samples/apps/iosched/Config.java
+++ android/src/main/java/com/saarang/samples/apps/iosched/Config.java
@@ -32,7 +32,7 @@ public class Config {
 
     // Warning messages for dogfood build
     public static final String DOGFOOD_BUILD_WARNING_TITLE = "Test build";
-    public static final String DOGFOOD_BUILD_WARNING_TEXT = "This is a test build.";
+    public static final String DOGFOOD_BUILD_WARNING_TEXT = "This is a double test build.";
 
     // Public data manifest URL
     public static final String PROD_CONFERENCE_DATA_MANIFEST_URL = "http://erp.saarang.org/static/json/manifest.json";
