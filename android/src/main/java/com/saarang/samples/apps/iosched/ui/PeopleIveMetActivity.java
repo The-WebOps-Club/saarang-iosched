41aef2170213516d13177e19d743c3fa236db74d
cooliscool <donajmal@gmail.com>
2015-12-26 21:56:05 +0530

    contacts

--- android/src/main/java/com/saarang/samples/apps/iosched/ui/PeopleIveMetActivity.java
+++ android/src/main/java/com/saarang/samples/apps/iosched/ui/PeopleIveMetActivity.java
@@ -24,18 +24,174 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.saarang.samples.apps.iosched.R;
+import com.saarang.samples.apps.iosched.util.ContactObject;
 import com.saarang.samples.apps.iosched.util.PrefUtils;
 
 public class PeopleIveMetActivity extends BaseActivity {
 
     private static final String FRAGMENT_PEOPLE_IVE_MET = "people_ive_met";
     public static final int REQUEST_RESOLUTION_FOR_RESULT = 1;
+
+
+
+    ContactObject con1 = new ContactObject(
+            "Aditya U",
+            "adi@saarang.org",
+            "+919962971662",
+            "ayoo",
+            R.drawable.p_adi);
+    ContactObject con2 = new ContactObject(
+            "Krishna Koushik",
+            "krishnakoushik@saarang.org",
+            "+917418789160",
+            "ayoo",
+            R.drawable.p_krishnakoushik);
+    ContactObject con3 = new ContactObject(
+            "Anish Rathi",
+            "anish.rathi@saarang.org",
+            "+919884685016",
+            "ayoo",
+            R.drawable.p_anish);
+    ContactObject con4 = new ContactObject(
+            "Anirvan Bordoloi",
+            "anirvan.bordoloi@saarang.org",
+            "+919962609617",
+            "ayoo",
+            R.drawable.p_anirvan);
+    ContactObject con5 = new ContactObject(
+            "Ashish Jha",
+            "ashish.jha@saarang.org",
+            "+918695851252",
+            "ayoo",
+            R.drawable.p_aashish);
+    ContactObject con6 = new ContactObject(
+            "Rushabh Menon",
+            "menon@saarang.org",
+            "+919791057815 ",
+            "ayoo",
+            R.drawable.p_rushabh);
+    ContactObject con7 = new ContactObject(
+            "Srijith R",
+            "srijith@saarang.org",
+            "+919176467000",
+            "ayoo",
+            R.drawable.p_srijith);
+    ContactObject con8 = new ContactObject(
+            "Ashwin S Pothen",
+            "ashwin@saarang.org",
+            "+919884300360",
+            "ayoo",
+            R.drawable.p_pothen);
+    ContactObject con9 = new ContactObject(
+            "Harshith Guntha",
+            "harshith.guntha@saarang.org",
+            "+918056218712 ",
+            "ayoo",
+            R.drawable.p_harshith);
+    ContactObject con10 = new ContactObject(
+            "Sai Krishna Koushik",
+            "koushik.esk@saarang.org",
+            "+918124343670",
+            "ayoo",
+            R.drawable.p_saikrishnakoushik);
+    ContactObject con11 = new ContactObject(
+            "Sai Akhil Matha",
+            "akhil@saarang.org",
+            "+918056128354",
+            "ayoo",
+            R.drawable.p_akhil);
+    ContactObject con12 = new ContactObject(
+            "Rinkesh Virani",
+            "rinkeshvirani@saarang.org",
+            "+919884299313",
+            "ayoo",
+            R.drawable.p_rinkesh);
+    ContactObject con13 = new ContactObject(
+            "Sreeharsha Gunda",
+            "sreeharsha@saarang.org",
+            "+919962663576",
+            "ayoo",
+            R.drawable.p_sreeharsha);
+    ContactObject con14 = new ContactObject(
+            "Deepak Padamata",
+            "deepak@saarang.org",
+            "+919789107938",
+            "ayoo",
+            R.drawable.p_deepak);
+    ContactObject con15 = new ContactObject(
+            "Aqel Ahammed",
+            "aqel@saarang.org",
+            "+919633229144",
+            "ayoo",
+            R.drawable.p_aqel);
+    ContactObject con16 = new ContactObject(
+            "Manavala Thambi",
+            "ktmanav@saarang.org",
+            "+919962605305",
+            "ayoo",
+            R.drawable.p_thambi);
+    ContactObject con17 = new ContactObject(
+            "Rathees P",
+            "ratiz@saarang.org",
+            "+919789575877",
+            "ayoo",
+            R.drawable.p_rathees);
+    ContactObject con18 = new ContactObject(
+            "Rahul K",
+            "rahul@saarang.org",
+            "+919884299695",
+            "ayoo",
+            R.drawable.p_rahul);
+    ContactObject con19 = new ContactObject(
+            "Favas M P",
+            "favas@saarang.org",
+            "+917418304010",
+            "ayoo",
+            R.drawable.p_favas);
+    ContactObject con20 = new ContactObject(
+            "Srinivas Ramanand",
+            "ramanand@saarang.org",
+            "+918807488931",
+            "ayoo",
+            R.drawable.p_ramanand);
+    ContactObject con21 = new ContactObject(
+            "Hari M",
+            "hari@saarang.org",
+            "+917708948827",
+            "ayoo",
+            R.drawable.p_hari);
+    ContactObject con22 = new ContactObject(
+            "Amar",
+            "amar@saarang.org",
+            "+919952912020",
+            "ayoo",
+            R.drawable.p_amar);
+    ContactObject con23 = new ContactObject(
+            "Bharadwaj M",
+            "bharadwaj@saarang.org",
+            "+918056102507",
+            "ayoo",
+            R.drawable.p_bharadwaj);
+    ContactObject con24 = new ContactObject(
+            "Ruth Babu Marpu",
+            "ruth.marpu@saarang.org",
+            "+919043812280",
+            "ayoo",
+            R.drawable.p_ruthbabu);
+    ContactObject con25 = new ContactObject(
+            "Vidyadhar Mudium",
+            "vidyadhar@saarang.org",
+            "+919952044531",
+            "ayoo",
+            R.drawable.p_vidyadhar);
+
+
+    public Boolean isExpanded = false;
+    public View expLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,44 +201,56 @@ public class PeopleIveMetActivity extends BaseActivity {
         }
 
 
+        ContactObject[] contactCulsec1 = {con1};
+        ContactObject[] contactCulsec2 = {con2};
+        ContactObject[] contactSpons = {con3,con4,con5};
+        ContactObject[] contactEvents = {con6, con7};
+        ContactObject[] contactProshows = {con8,con9};
+        ContactObject[] contactMarketing = {con10, con11};
+        ContactObject[] contactPublicity = {con12};
+        ContactObject[] contactHospitality = {con13};
+        ContactObject[] contactMobops = {con14,con15};
+        ContactObject[] contactDesign = {con16, con17, con18};
+        ContactObject[] contactFinance = {con19, con20};
+        ContactObject[] contactFacilities = {con21, con22};
+        ContactObject[] contactSecurity = {con23, con24};
+        ContactObject[] contactQMS = {con25};
 
+        int[] llSpons={R.id.llSponsPerson1, R.id.llSponsPerson2, R.id.llSponsPerson3};
+        int[] llEvents={R.id.llEvents1, R.id.llEvents2};
+        int[] llPro={R.id.llPro1, R.id.llPro2};
+        int[] llMarketing={R.id.llMarketing1, R.id.llMarketing2};
+        int[] llPublicity={R.id.llPublicity1};
+        int[] llHospitality={R.id.llHospitality1};
+        int[] llMobops={R.id.llMobops1, R.id.llMobops2};
+        int[] llDesign={R.id.llDesign1, R.id.llDesign2, R.id.llDesign3};
+        int[] llFinance={R.id.llFinance1, R.id.llFinance2};
+        int[] llFacilities={R.id.llFacilities1, R.id.llFacilities2};
+        int[] llSecurity={R.id.llSecurity1, R.id.llSecurity2};
+        int[] llQMS={R.id.llQMS1};
+        int[] llCulsec1= {R.id.llCulsec1};
+        int[] llCulsec2= {R.id.llCulsec2};
 
         setContentView(com.saarang.samples.apps.iosched.R.layout.activity_people_ive_met);
         CardView bug = (CardView) findViewById(R.id.card_view);
         CardView rate = (CardView) findViewById(R.id.card_view1);
-        contacts(R.id.layout1,R.id.layout_exp_1,R.id.imageView1,"+919444005768");
-        contacts(R.id.layout2,R.id.layout_exp_2,R.id.imageView2,"+919444005769");
 
-        CardView mLayout1 = (CardView) findViewById(R.id.layout3);
-        LinearLayout mLayoutToBeExpanded1 = (LinearLayout) findViewById(R.id.layout_exp_31);
-
-        addAction1(mLayout1, mLayoutToBeExpanded1);
-        collapse(mLayoutToBeExpanded1);
-        ImageView imagecall31 =(ImageView) findViewById(R.id.imageView31);
-
-        imagecall31.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialPhoneNumber("+914422579815");
-            }
-        });
-        ImageView imagecall32 =(ImageView) findViewById(R.id.imageView31);
-
-        imagecall32.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialPhoneNumber("+914422579816");
-            }
-        });
-
-        contacts2(R.id.layout4,R.id.layout_exp_41,R.id.imageView41,R.id.imageView42,"+919677169792","+919444357587");
-        contacts2(R.id.layout5,R.id.layout_exp_51,R.id.imageView51,R.id.imageView52,"+919962301415","+919952993777");
-        contacts2(R.id.layout6,R.id.layout_exp_61,R.id.imageView61,R.id.imageView62,"+918754557441","+919962252801");
-        contacts2(R.id.layout7,R.id.layout_exp_71,R.id.imageView71,R.id.imageView72,"+914422579810","+914422579811");
-
-
-
-
+        addPopupContactN(1, R.id.layout1, R.id.layout_exp_1, llCulsec1, contactCulsec1,200);
+        addPopupContactN(1, R.id.layout2, R.id.layout_exp_2, llCulsec2, contactCulsec2, 200);
+        addCallContact(R.id.layout3, R.id.layout_exp_31, R.id.linearLayoutGod, R.id.linearLayoutSha, "+914422579815", "+914422579816");
+        addCallContact(R.id.layout7, R.id.layout_exp_71, R.id.linearLayoutHelp1, R.id.linearLayoutHelp2, "+914422579810", "+914422579811");
+        addPopupContactN(2, R.id.layout4, R.id.layout_exp_41, llEvents, contactEvents, 200);
+        addPopupContactN(2, R.id.layout5, R.id.layout_exp_51, llPro, contactProshows, 200);
+        addPopupContactN(2, R.id.layout6, R.id.layout_exp_61,llMarketing, contactMarketing,200);
+        addPopupContactN(3, R.id.cardLayoutSpons, R.id.llSpons, llSpons, contactSpons , 400);
+        addPopupContactN(3, R.id.cardLayoutDesign, R.id.llDesign, llDesign, contactDesign , 400);
+        addPopupContactN(2, R.id.cardLayoutMobops, R.id.llMobops,llMobops, contactMobops,200);
+        addPopupContactN(2, R.id.cardLayoutFinance, R.id.llFinance,llFinance, contactFinance,200);
+        addPopupContactN(2, R.id.cardLayoutFacilities, R.id.llFacilities,llFacilities, contactFacilities,200);
+        addPopupContactN(2, R.id.cardLayoutSecurity, R.id.llSecurity,llSecurity, contactSecurity,200);
+        addPopupContactN(1, R.id.cardLayoutPublicity, R.id.rlPublicity, llPublicity, contactPublicity, 200);
+        addPopupContactN(1, R.id.cardLayoutHospitality, R.id.rlHospitality, llHospitality, contactHospitality, 200);
+        addPopupContactN(1, R.id.cardLayoutQMS, R.id.rlQMS, llQMS, contactQMS, 200);
 
 
         bug.setOnClickListener(new View.OnClickListener() {
@@ -90,8 +258,6 @@ public class PeopleIveMetActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 String[] email = {"webadmin@saarang.org"};
-
-
                 composeEmail(email, "Saarang android app bug report") ;
 
             }
@@ -149,6 +315,64 @@ public class PeopleIveMetActivity extends BaseActivity {
         }
     }
 
+    public void addCallContact(int cardview, int listview, int lla, int llb,final String num1, final String num2){
+        CardView mLayout1 = (CardView) findViewById(cardview);
+        LinearLayout mLayoutToBeExpanded1 = (LinearLayout) findViewById(listview);
+
+        addAction(mLayout1, mLayoutToBeExpanded1, 200);
+        collapse(mLayoutToBeExpanded1);
+
+        LinearLayout ll1 = (LinearLayout) findViewById(lla);
+        LinearLayout ll2 = (LinearLayout) findViewById(llb);
+        ll1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(num1);
+            }
+        });
+        ll2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(num2);
+            }
+        });
+
+    }
+
+
+
+    public void addPopupContactN(int num, int cardview, int listview, int[] llList, final ContactObject[] c, int height){
+
+
+        CardView mLayout1 = (CardView) findViewById(cardview);
+        if(num==1){
+            RelativeLayout mLayoutToBeExpanded1 = (RelativeLayout) findViewById(listview);
+            addAction(mLayout1, mLayoutToBeExpanded1, height);
+            collapse(mLayoutToBeExpanded1);
+        }
+        else {
+            LinearLayout mLayoutToBeExpanded1 = (LinearLayout) findViewById(listview);
+            addAction(mLayout1, mLayoutToBeExpanded1, height);
+            collapse(mLayoutToBeExpanded1);
+        }
+
+
+
+        for(int i=0; i<num; i++){
+            LinearLayout ll = (LinearLayout) findViewById(llList[i]);
+            final int sase = i;
+            ll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(PeopleIveMetActivity.this, ContactDialogActivity.class);
+                    in.putExtra("details", c[sase]);
+                    startActivity(in);
+                }
+            });
+        }
+
+    }
+
     public void composeEmail(String[] addresses, String subject) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
@@ -158,24 +382,7 @@ public class PeopleIveMetActivity extends BaseActivity {
             startActivity(intent);
         }
     }
-    public void addAction(final View layout, final View summary) {
-
-
-
-        layout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (summary.getVisibility() == View.GONE) {
-                    expand(summary);
-                } else {
-                    collapse(summary);
-                }
-            }
-        });
-    }
-
-    private void expand(View summary) {
+    private void expand(View summary, int value) {
         //set Visible
         int finalHeight = summary.getHeight();
 
@@ -186,42 +393,45 @@ public class PeopleIveMetActivity extends BaseActivity {
         final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
         summary.measure(widthSpec, 300);
 
-        mAnimator = slideAnimator(0, 150, summary);
+        mAnimator = slideAnimator(0, value, summary);
 
         mAnimator.start();
     }
-    public void addAction1(final View layout, final View summary) {
 
-
+    public void addAction(final View layout, final View summary, final int value) {
 
         layout.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 if (summary.getVisibility() == View.GONE) {
-                    expand1(summary);
+
+
+                    if(isExpanded){
+                        collapse(expLayout);
+
+                    }
+                    else{
+                        isExpanded = true;
+                    }
+
+                    expLayout = summary;
+                    expand(summary, value);
+
                 } else {
+
+                    if(isExpanded){
+                        isExpanded = false;
+
+                    }
+
                     collapse(summary);
                 }
             }
         });
     }
 
-    private void expand1(View summary) {
-        //set Visible
-        int finalHeight = summary.getHeight();
 
-        ValueAnimator mAnimator ;
-
-        summary.setVisibility(View.VISIBLE);
-
-        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
-        summary.measure(widthSpec, 300);
-
-        mAnimator = slideAnimator(0, 260, summary);
-
-        mAnimator.start();
-    }
     private void collapse(final View summary) {
         int finalHeight = summary.getHeight();
 
@@ -275,41 +485,4 @@ public class PeopleIveMetActivity extends BaseActivity {
             startActivity(intent);
         }
     }
-    public  void contacts(int cardview,int listview,int imageview, final String number){
-        CardView mLayout1 = (CardView) findViewById(cardview);
-        RelativeLayout mLayoutToBeExpanded1 = (RelativeLayout) findViewById(listview);
-        ImageView imagecall =(ImageView) findViewById(imageview);
-        addAction(mLayout1, mLayoutToBeExpanded1);
-        collapse(mLayoutToBeExpanded1);
-        imagecall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialPhoneNumber(number);
-            }
-        });
-    }
-    public  void contacts2(int cardview,int listview,int imageview1,int imageview2, final String number1,final String number2){
-        CardView mLayout1 = (CardView) findViewById(cardview);
-        LinearLayout mLayoutToBeExpanded1 = (LinearLayout) findViewById(listview);
-
-        addAction1(mLayout1, mLayoutToBeExpanded1);
-        collapse(mLayoutToBeExpanded1);
-        ImageView imagecall31 =(ImageView) findViewById(imageview1);
-
-        imagecall31.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialPhoneNumber(number1);
-            }
-        });
-        ImageView imagecall32 =(ImageView) findViewById(imageview2);
-
-        imagecall32.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialPhoneNumber(number2);
-            }
-        });
-    }
-
 }
